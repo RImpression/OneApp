21927dd1eed7f86ab6a69741b1254733acd3db45
RImpression <120769625@qq.com>
2016-03-31 18:57:27 +0800

    实现轮播图片功能

--- app/src/main/java/com/example/oneapp/fragment/FragmentArticle.java
+++ app/src/main/java/com/example/oneapp/fragment/FragmentArticle.java
@@ -1,15 +1,19 @@
 package com.example.oneapp.fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.android.volley.VolleyError;
@@ -19,6 +23,7 @@ import com.example.entity.GuideReadingEntity;
 import com.example.https.MyRequest;
 import com.example.interfaces.HttpListener;
 import com.example.oneapp.R;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -26,6 +31,9 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by RImpression on 2016/3/20 0020.
@@ -41,11 +49,43 @@ public class FragmentArticle extends Fragment {
     private List<ArticleEntity> mDataList = null;
 
 
+    private ViewPager adViewPager;
+    private List<ImageView> imageViews;// 滑动的图片集合
+    private List<View> dots; // 图片标题正文的那些点
+    private List<View> dotList;
+
+    private int currentItem = 0; // 当前图片的索引号
+    // 定义的五个指示点
+    private View dot0;
+    private View dot1;
+    private View dot2;
+    private View dot3;
+    private View dot4;
+    private View dot5;
+    private View dot6;
+    private View dot7;
+    private View dot8;
+
+    // 定时任务
+    private ScheduledExecutorService scheduledExecutorService;
+
+    // 轮播banner的数据
+    private List<GuideReadingEntity> adList = null;
+
+    private Handler handler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+            adViewPager.setCurrentItem(currentItem);
+        };
+    };
+
+
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (isFirst == true){
             getArticleRequest();
+            getPhotoRequest();
         }
     }
 
@@ -60,9 +100,16 @@ public class FragmentArticle extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        mRecyclerView = (RecyclerView) getView().findViewById(R.id.recyclerView);
+
         initView();
+        startAd();
         if (isFirst == false){
             loadRecycleView(mDataList);
+            adViewPager.setAdapter(new MyAdapter());// 设置填充ViewPager页面的适配器
+            // 设置一个监听器，当ViewPager中的页面改变时调用
+            adViewPager.setOnPageChangeListener(new MyPageChangeListener());
+            addDynamicView();
         }
 
     }
@@ -70,8 +117,175 @@ public class FragmentArticle extends Fragment {
 
 
     private void initView() {
-        mRecyclerView = (RecyclerView) getView().findViewById(R.id.recyclerView);
+
+        imageViews = new ArrayList<ImageView>();
+        // 点
+        dots = new ArrayList<View>();
+        dotList = new ArrayList<View>();
+        dot0 = getView().findViewById(R.id.v_dot0);
+        dot1 = getView().findViewById(R.id.v_dot1);
+        dot2 = getView().findViewById(R.id.v_dot2);
+        dot3 = getView().findViewById(R.id.v_dot3);
+        dot4 = getView().findViewById(R.id.v_dot4);
+        dot5 = getView().findViewById(R.id.v_dot5);
+        dot6 = getView().findViewById(R.id.v_dot6);
+        dot7 = getView().findViewById(R.id.v_dot7);
+        dot8 = getView().findViewById(R.id.v_dot8);
+        dots.add(dot0);
+        dots.add(dot1);
+        dots.add(dot2);
+        dots.add(dot3);
+        dots.add(dot4);
+        dots.add(dot5);
+        dots.add(dot6);
+        dots.add(dot7);
+        dots.add(dot8);
+
+        adViewPager = (ViewPager) getView().findViewById(R.id.vp);
+
+
     }
+
+    private void addDynamicView() {
+        // 动态添加图片和下面指示的圆点
+        // 初始化图片资源
+        for (int i = 0; i < adList.size(); i++) {
+            ImageView imageView = new ImageView(getContext());
+            // 异步加载图片
+            Picasso.with(getContext()).load(adList.get(i).getCover()).resize(250,113).centerCrop().into(imageView);
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageViews.add(imageView);
+            dots.get(i).setVisibility(View.VISIBLE);
+            dotList.add(dots.get(i));
+        }
+
+    }
+
+    private void startAd() {
+        scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+        // 当Activity显示出来后，每两秒切换一次图片显示
+        scheduledExecutorService.scheduleAtFixedRate(new ScrollTask(), 1, 5,
+                TimeUnit.SECONDS);
+    }
+
+    private class ScrollTask implements Runnable {
+
+        @Override
+        public void run() {
+            synchronized (adViewPager) {
+                currentItem = (currentItem + 1) % imageViews.size();
+                handler.obtainMessage().sendToTarget();
+            }
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        // 当Activity不可见的时候停止切换
+        scheduledExecutorService.shutdown();
+    }
+
+    private class MyPageChangeListener implements ViewPager.OnPageChangeListener {
+
+        private int oldPosition = 0;
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            currentItem = position;
+            GuideReadingEntity entity = adList.get(position);
+
+            dots.get(oldPosition).setBackgroundResource(R.drawable.dot_normal);
+            dots.get(position).setBackgroundResource(R.drawable.dot_focused);
+            oldPosition = position;
+        }
+    }
+
+    private class MyAdapter extends PagerAdapter {
+
+//        private List<ImageView> imageViews = new ArrayList<>();
+//        private List<GuideReadingEntity> adList = new ArrayList<>();
+//
+//
+//        public MyAdapter(List<ImageView> imageViews,List<GuideReadingEntity> adList){
+//            this.imageViews = imageViews;
+//            this.adList = adList;
+//        }
+
+        @Override
+        public int getCount() {
+            return adList.size();
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            Log.i("adList",adList.get(0).getTitle()+"444444444444"+position);
+            Log.i("adList",imageViews.toString()+"444444444444"+position);
+            ImageView iv = imageViews.get(position);
+            ((ViewPager) container).addView(iv);
+            final GuideReadingEntity adDomain = adList.get(position);
+            // 在这个方法里面设置图片的点击事件
+            iv.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    // 处理跳转逻辑
+                }
+            });
+            return iv;
+        }
+
+
+
+        @Override
+        public void destroyItem(View arg0, int arg1, Object arg2) {
+            ((ViewPager) arg0).removeView((View) arg2);
+        }
+
+        @Override
+        public boolean isViewFromObject(View arg0, Object arg1) {
+            return arg0 == arg1;
+        }
+
+        @Override
+        public void restoreState(Parcelable arg0, ClassLoader arg1) {
+
+        }
+
+        @Override
+        public Parcelable saveState() {
+            return null;
+        }
+
+        @Override
+        public void startUpdate(View arg0) {
+
+        }
+
+        @Override
+        public void finishUpdate(View arg0) {
+
+        }
+
+    }
+
+
+
+
+
+
+
+
 
     /**
      * 请求轮播图片数据
@@ -80,7 +294,12 @@ public class FragmentArticle extends Fragment {
         new MyRequest(getContext().getApplicationContext()).getRequest(URL_PHTOT, new HttpListener() {
             @Override
             public void onSuccess(String result) {
-                parse2PhotoJson(result);
+                adList = parse2PhotoJson(result);
+                addDynamicView();
+                adViewPager.setAdapter(new MyAdapter());// 设置填充ViewPager页面的适配器
+                // 设置一个监听器，当ViewPager中的页面改变时调用
+                adViewPager.setOnPageChangeListener(new MyPageChangeListener());
+
             }
 
             @Override
